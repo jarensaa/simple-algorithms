32cf0182802834471cd0c5223efca205a99976e9
jarensaa <jens.rensaa@gmail.com>
2016-12-25 18:35:24 +0100

    Started on sieve

---
+++ prime-number-algorithms/sieve-algorithms/sieve.java
@@ -0,0 +1,8 @@
+/*
+Created by Jens-Andreas Rensaa, 25.12.16
+
+The following program uses a basic implementation of sieve to find all primes not greater than n.
+
+Input - the number n
+Output - All primes from 1 to and including n.
+ */
